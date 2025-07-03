58944ab6ba27b3124a73ce22dcfbe8b6c2816efc
Alexis <agvillacorta2001@gmail.com>
2025-07-03 14:21:56 -0300

    Creando el primer commit

---
+++ test/ar/edu/unlam/pb2/cazadoresTest/CazadorTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.cazadoresTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CazadorTest {
+
+	@Test
+	public void queSeCreeElCazador() {
+		Cazador miCazador = new Cazador();
+	}
+
+}
